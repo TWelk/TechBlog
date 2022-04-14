fef9e6b8e118f47f7491c7341887caab713fd587
Tyler Welker <twelker08@gmail.com>
2022-04-13 23:05:13 -0400

    adding toggle switch

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,7 +1,9 @@
 const body = document.querySelector('body');
+const postpage = document.getElementById('postpage')
 const toggle = document.getElementById('toggle');
 
 toggle.onclick = function() {
     toggle.classList.toggle('active');
     body.classList.toggle('active');
+    postpage.classList.toggle('active')
 }
